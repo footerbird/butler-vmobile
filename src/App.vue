ceb5519aa0b301e57a87b2fbd60014b46b6a45a2
footerbird <footerbird@163.com>
2020-03-25 00:02:25 +0800

    adjust base css place

--- src/App.vue
+++ src/App.vue
@@ -9,5 +9,39 @@
 </script>
 
 <style>
+* {
+  margin:0;
+  padding:0;
+}
+html {
+  font-size:50px;
+}
+html,body {
+  height:100%;
+}
+body {
+  font-size:16px;
+  color:#333;
+  font-family:-apple-system-font,arial,verdana,helvetica,'PingFang SC','HanHei SC',
+  STHeitiSC-Light,Microsoft Yahei,sans-serif;
+  background:#f7f7f7;
+}
+#app {
+  height:100%;
+}
+li {
+  list-style:none;
+}
+img {
+  display:block;
+}
+address,cite,dfn,em,var,i {
+  font-style:normal;
+}
+/* 将斜体扶正 */
 
+input,textarea {
+  font-family:-apple-system-font,arial,verdana,helvetica,'PingFang SC','HanHei SC',
+  STHeitiSC-Light,Microsoft Yahei,sans-serif;
+}
 </style>
