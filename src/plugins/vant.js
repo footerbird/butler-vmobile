1b45dca404079a893c4049e2cc3af397ce7e9cec
footerbird <footerbird@163.com>
2020-03-29 14:50:03 +0800

    vant 按需引入组件

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,6 +1,30 @@
 import Vue from 'vue';
-import Vant from 'vant';
+// import Vant from 'vant';
+import {
+  Button,
+  Cell,
+  CellGroup,
+  Popup,
+  Field,
+  Loading,
+  Toast,
+  List,
+  PullRefresh,
+  Swipe,
+  SwipeItem,
+} from 'vant';
 import 'vant/lib/index.css';
 import '../vant-variables.less';
 
-Vue.use(Vant);
+// Vue.use(Vant);
+Vue.use(Button);
+Vue.use(Cell);
+Vue.use(CellGroup);
+Vue.use(Popup);
+Vue.use(Field);
+Vue.use(Loading);
+Vue.use(Toast);
+Vue.use(List);
+Vue.use(PullRefresh);
+Vue.use(Swipe);
+Vue.use(SwipeItem);
