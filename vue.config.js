be06860b1488a7f68de942bff544f99e505ce813
footerbird <footerbird@163.com>
2020-03-29 17:31:35 +0800

    egg server init and change vue dist path

--- vue.config.js
+++ vue.config.js
@@ -9,15 +9,16 @@ module.exports = {
       ],
     },
   },
+  outputDir: 'server/dist', // 打包文件输出目录,默认'dist'
   devServer: {
     // 改完代理配置需要重新npm run serve
     proxy: {
       '/api': {
         target: 'https://m.waitui.com/api',
-        ws: true,// proxy websockets
-        changeOrigin: true,// needed for virtual hosted sites
-        pathRewrite: {//// rewrite path 将本地请求转发到代理地址
-            '^/api': ''
+        ws: true, // proxy websockets
+        changeOrigin: true, // needed for virtual hosted sites
+        pathRewrite: { // rewrite path 将本地请求转发到代理地址
+          '^/api': '',
         },
       },
     },
